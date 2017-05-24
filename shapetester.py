b8c6418b68481ce6631f02840831a227ba91f8bf
James Ninow <jamenino9632@granitesd.org>
2017-05-24 11:28:18 -0600

    Add files via upload

---
+++ shapetester.py
@@ -0,0 +1,14 @@
+class Box:
+   'welcome to shape maker! lest find volume and surface area for a few shapes'
+   boxCount = 0
+
+   def __init__(self, length, hight):
+      self.length = length
+      self.height = height
+   
+   def displayCount(self):
+     print "The volume for your box:" % box.boxCount
+
+   def displayEmployee(self):
+      print " length: ", self.length,  ", height: ", self.height
+    
